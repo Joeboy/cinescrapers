78cfa2bb7594976095da7975e51b7a86eaa0e84c
Joe Button <j.button@reckondigital.com>
2025-06-25 16:01:01 +0100

    Add dev server for testing the react app with local data

---
+++ devserver.py
@@ -0,0 +1,19 @@
+"""Just a little dev server for serving the data locally for testing purposes"""
+
+from pathlib import Path
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+
+@app.route("/cinescrapers.json")
+def serve_gzipped_json():
+    path = Path(__file__).parent / "src" / "cinescrapers" / "cinescrapers.json"
+    response = send_file(path, mimetype="application/json")
+    response.headers['Content-Type'] = 'application/json'
+    response.headers["Content-Encoding"] = "gzip"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+app.run(host="0.0.0.0", port=8080)
