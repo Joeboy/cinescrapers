06356016a76309290107b919fd028c82724f8902
Joe Button <j.button@reckondigital.com>
2025-07-10 01:09:50 +0100

    Make devserver serve thumbnail files

--- devserver.py
+++ devserver.py
@@ -1,7 +1,11 @@
 """Just a little dev server for serving the data locally for testing purposes"""
 
 from pathlib import Path
-from flask import Flask, send_file
+from flask import Flask, send_file, send_from_directory
+
+THUMBNAILS_DIR = (
+    Path(__file__).parent / "src" / "cinescrapers" / "scraped_images" / "thumbnails"
+)
 
 app = Flask(__name__)
 
@@ -24,6 +28,11 @@ def serve_cinemas():
     response.headers["Content-Encoding"] = "gzip"
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
+
+
+@app.route("/thumbnails/<path:filename>")
+def serve_thumbnail(filename):
+    return send_from_directory(THUMBNAILS_DIR, filename)
 
 
 app.run(host="0.0.0.0", port=8080)
