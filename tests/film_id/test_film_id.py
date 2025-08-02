e59f8e9eea7f31d34706fd62de5c7a1cf60e6ad0
Joe Button <j.button@reckondigital.com>
2025-08-02 16:35:26 +0100

    Fix test

--- tests/film_id/test_film_id.py
+++ tests/film_id/test_film_id.py
@@ -5,6 +5,7 @@ import time
 import pytest
 from rich import print
 
+from cinescrapers.cinescrapers_types import EnrichedShowTime
 from cinescrapers.film_identification import get_best_tmdb_match
 
 TESTDATA_ROOT = Path(__file__).parent / "test_data"
@@ -18,17 +19,18 @@ def showtimes_data():
     with sqlite3.connect(DB_PATH) as conn:
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        cursor.execute(
-            "SELECT distinct title, norm_title, description, thumbnail FROM showtimes LIMIT 100"
-        )
-        yield [dict(row) for row in cursor.fetchall()]
+        cursor.execute("SELECT * FROM showtimes LIMIT 100")
+        yield (EnrichedShowTime(**dict(row)) for row in cursor.fetchall())
 
 
 def test_film_id(showtimes_data):
-    """None of this is actually used and this isn't really a test"""
+    """This isn't a very good test, but it gives us a way to run the code"""
 
+    num_results = 0
     num_matches = 0
+
     for showtime_result in showtimes_data:
+        num_results += 1
         print("-" * 80)
         print(showtime_result)
         match = get_best_tmdb_match(showtime_result, IMAGES_CACHE)
@@ -40,4 +42,4 @@ def test_film_id(showtimes_data):
             print("NO MATCH")
         time.sleep(0.1)
 
-    print(f"Total matches found: {num_matches} out of {len(showtimes_data)}")
+    print(f"Total matches found: {num_matches} out of {num_results}")
