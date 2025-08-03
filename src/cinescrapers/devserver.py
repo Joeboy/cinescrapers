0290879e5616bfffef7c3bb1e9cd92c6d9f437f8
Joe Button <j.button@reckondigital.com>
2025-08-03 11:07:40 +0100

    Add stats page to devserver using ydata-stats package
    Not sure how useful this will be but it's simple and kind of cool

--- src/cinescrapers/devserver.py
+++ src/cinescrapers/devserver.py
@@ -1,7 +1,11 @@
 """Just a little dev server for serving the data locally for testing purposes"""
 
+import sqlite3
 from pathlib import Path
+
+import pandas as pd
 from flask import Flask, send_file, send_from_directory
+from ydata_profiling import ProfileReport
 
 THUMBNAILS_DIR = (
     Path(__file__).parent / "src" / "cinescrapers" / "scraped_images" / "thumbnails"
@@ -32,7 +36,13 @@ def serve_cinemas():
 
 @app.route("/tmdb_recommendations.json")
 def serve_tmdb_recommendations():
-    path = Path(__file__).parent / "src" / "cinescrapers" / "data" / "tmdb_recommendations.json"
+    path = (
+        Path(__file__).parent
+        / "src"
+        / "cinescrapers"
+        / "data"
+        / "tmdb_recommendations.json"
+    )
     response = send_file(path, mimetype="application/json")
     response.headers["Content-Type"] = "application/json"
     # response.headers["Content-Encoding"] = "gzip"
@@ -43,6 +53,15 @@ def serve_tmdb_recommendations():
 @app.route("/thumbnails/<path:filename>")
 def serve_thumbnail(filename):
     return send_from_directory(THUMBNAILS_DIR, filename)
+
+
+@app.route("/stats")
+def serve_stats():
+    DB_PATH = Path(__file__).parent.parent / "showtimes.db"
+    with sqlite3.connect(DB_PATH) as conn:
+        df = pd.read_sql_query("SELECT * FROM showtimes", conn)
+    profile = ProfileReport(df, title=f"Data Profile", minimal=True)
+    return profile.to_html(), 200
 
 
 app.run(host="0.0.0.0", port=8080)
